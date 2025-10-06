d927762967c94815d2cbe9234598c0c9ef7f79c3
palitojendthen <palitoj.endthen@outlook.com>
2025-10-07 00:02:48 +0700

    update layout.js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,32 +1,4 @@
-﻿// import { ThemeProvider } from "next-themes";
-// import "./globals.css";
-
-// export const metadata = {
-//   title: "ApexQuantLabs",
-//   description: "Quantitative Research and Backtesting Platform",
-// };
-
-// // Make a small wrapper client component for ThemeProvider
-// function ThemeWrapper({ children }) {
-//   "use client";
-//   return (
-//     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-//       {children}
-//     </ThemeProvider>
-//   );
-// }
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <body>
-//         <ThemeWrapper>{children}</ThemeWrapper>
-//       </body>
-//     </html>
-//   );
-// }
-
-import Navbar from "./components/Navbar";
+﻿import Navbar from "./components/Navbar";
 import "./globals.css";
 import Providers from "./providers";
 
