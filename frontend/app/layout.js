8722f2c2a41398a12f28db233010d7c34e2c880f
palitojendthen <palitoj.endthen@outlook.com>
2025-10-07 11:44:26 +0700

    update layout.js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,26 +1,4 @@
-﻿// import Navbar from "./components/Navbar";
-// import "./globals.css";
-// import Providers from "./providers";
-
-// export const metadata = {
-//   title: "ApexQuantLabs",
-//   description: "Quantitative Research & Backtesting Platform",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <body className="min-h-screen bg-white text-black dark:bg-black dark:text-white transition-colors duration-300">
-//         <Providers>
-//           <Navbar />
-//           <main className="pt-20">{children}</main>
-//         </Providers>
-//       </body>
-//     </html>
-//   );
-// }
-
-import BackgroundFX from "./components/BackgroundFX";
+﻿import BackgroundFX from "./components/BackgroundFX";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 import Providers from "./providers";
