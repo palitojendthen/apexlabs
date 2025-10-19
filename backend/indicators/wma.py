c63b45d54a0c68ececf03b71b635b5f59c5949d4
palitojendthen <palitoj.endthen@outlook.com>
2025-10-19 19:09:30 +0700

    update wma.py

--- backend/indicators/wma.py
+++ backend/indicators/wma.py
@@ -15,15 +15,19 @@ def wma(source_wma: pd.Series, n_wma=14):
     pd.Series
         wma values aligned with source_wma index
     """
-    src = src.dropna()
-    n = len(src)
-    
-    if n < periods:
+
+    if not isinstance(source_wma, pd.Series):
+        source_ema = pd.Series(source_wma)
+
+    source_wma = pd.to_numeric(source_wma, errors="coerce")
+
+    if len(source_wma) < n_wma:
         raise ValueError("Periods can't be greater than data length")
-    
-    w = np.arange(1, periods+1)
+
+    w = np.arange(1, n_wma+1)
     w_sum = w.sum()
     weights = w/w_sum
-    wma = src.rolling(window = periods).apply(lambda y: np.dot(y, weights), raw = True)    
-    
-    return wma
+    wma = source_wma.rolling(window = n_wma).apply(lambda y: np.dot(y, weights), raw = True)    
+    wma_series = pd.Series(wma, index=source_wma.index, dtype=float)
+
+    return wma_series
