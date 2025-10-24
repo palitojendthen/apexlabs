5389f1352779cf8d61d6ba1bc88258f5bcbfb385
palitojendthen <palitoj.endthen@outlook.com>
2025-10-24 19:58:14 +0700

    update adx.py

--- backend/indicators/adx.py
+++ backend/indicators/adx.py
@@ -4,36 +4,6 @@ import numpy as np
 import importlib.util
 from pathlib import Path
 
-# def atr(source_atr: pd.DataFrame, n_atr=10) -> pd.Series:
-#     """
-#     technical analysis indicator:
-#     return average true range, over a given dataframe
-#     reference: https://www.investopedia.com/terms/a/atr.asp
-#     params:
-#     @source_atr: DataFrame, ohlc input data
-#     @n_atr: integer, loockback period (default 10)
-#     returns:
-#         [true range, average true] aligned with source index
-#     """
-#     _src = source_atr.copy()
-#     n = len(_src)
-    
-#     if n < n_atr:
-#         raise ValueError('Periods cant be greater than data length')
-
-#     _src['hl'] = _src['high']-_src['low']
-#     _src['hc1'] = _src['high']-_src['close'].shift(1)
-#     _src['lc1'] = _src['low']-_src['close'].shift(1)
-#     _src['tr'] = .00
-
-#     for i in range(0, n):
-#         _src.iloc[i, _src.columns.get_loc('tr')] = np.max([_src.iloc[i, _src.columns.get_loc('hl')], _src.iloc[i, _src.columns.get_loc('hc1')], _src.iloc[i, _src.columns.get_loc('lc1')]], axis=0)
-    
-#     _src['atr'] = _src['tr'].rolling(window=n_atr).mean()
-    
-#     return _src[['tr', 'atr']]
-
-
 def load_atr_module():
     module_path = Path(__file__).resolve().parents[0]/"atr.py"
     spec = importlib.util.spec_from_file_location("atr", module_path)
